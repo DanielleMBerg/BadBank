77f7fba263466cfab0b606353a3e0682a04ad8b8
Danielle Berg <124546091+DanielleMBerg@users.noreply.github.com>
2023-09-25 14:56:43 -0700

    Update history.jsx

--- src/Components/history.jsx
+++ src/Components/history.jsx
@@ -1,19 +1,28 @@
 export const RecordTransactionHistory = ({currentUser}) => {
-    let transactionList;
-    if (currentUser) {
-      transactionList = currentUser.transactions.map((transaction, index) => {
-        return (
-            <tr key={index}>
-              <td>${transaction[0]}</td>
-              <td>{transaction[1]}</td>
-              <td>{transaction[2]}</td>
-            </tr>
-        )
-      })
-    }
+  let transactionList;
+  if (currentUser) {
+    transactionList = currentUser.transactions.map((transaction, index) => {
       return (
-        <>
-        {transactionList}
-        </>
+          <tr key={index}>
+            <td>${transaction[0]}</td>
+            <td>{transaction[1]}</td>
+            <td>{transaction[2]}</td>
+          </tr>
       )
-    }
+    })
+  }
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Amount</th>
+          <th>Account</th>
+          <th>Type</th>
+        </tr>
+      </thead>
+      <tbody>
+        {transactionList}
+      </tbody>
+    </table>
+  )
+}
